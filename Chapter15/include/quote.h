60a372469377b3d7f81d02cc721fc4fed56bd794
dingyanxu <dingyanxu@dreame.tech>
2024-06-16 17:27:50 +0800

    15.7

---
+++ Chapter15/include/quote.h
@@ -0,0 +1,69 @@
+#ifndef QUOTE_H
+#define QUOTE_H
+
+#include <string>
+#include <iostream>
+
+class Quote {
+public:
+  Quote() = default;
+  Quote(const std::string &book, double sales_price) : bookNo_(book), price_(sales_price) {}
+  std::string isbn() const { return bookNo_; }
+  virtual double net_price(size_t n) const { return n * price_; }
+  virtual void debug() {
+    std::cout << "bookNo=" << bookNo_ << " price=" << price_ << std::endl;
+  }
+  virtual ~Quote() = default;
+
+private:
+  std::string bookNo_;
+
+protected:
+  double price_ = 0.0;
+};
+
+class DiscQuote : public Quote {
+public:
+  DiscQuote() = default;
+  DiscQuote(const std::string &book, double price, size_t qty, double disc) : 
+    Quote(book, price), quantity_(qty), discount_(disc) { } 
+  double net_price(size_t n) const = 0;
+
+protected:
+  size_t quantity_ = 0;
+  double discount_ = 0.0;
+};
+
+class BulkQuote : public DiscQuote {
+public:
+  BulkQuote() = default;
+  BulkQuote(const std::string &book, double price, size_t qty, double disc) : 
+    DiscQuote(book, price, qty, disc) { } 
+  double net_price(size_t n) const override {
+    if (n >= quantity_) {
+      return n * (1 - discount_) * price_;
+    } else {
+      return n * price_;
+    }
+  }
+  void debug() override {
+    Quote::debug();
+    std::cout << "qty=" << quantity_ << " discount=" << discount_ << std::endl;
+  }
+};
+
+class LimitedQuote : public DiscQuote {
+public:
+  LimitedQuote() = default;
+  LimitedQuote(const std::string &book, double price, size_t qty, double disc) : 
+    DiscQuote(book, price, qty, disc) { } 
+  double net_price(size_t n) const override {
+    if (n <= quantity_) {
+      return n * ( 1- discount_ ) * price_;
+    } else {
+      return quantity_ * (1 - discount_) * price_ + (n - quantity_) * price_;
+    }
+  }
+};
+
+#endif
---
+++ Chapter15/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "quote.h"
+
+double print_total(std::ostream &os, const Quote &item, size_t n) {
+  double ret  = item.net_price(n);
+  os << "ISBN: " << item.isbn() << " # sold: " << n << " total due: " << ret << std::endl;
+  return ret;
+}
+
+int main(int argc, char *argv[]) {
+  Quote basic("c++ primer", 80.0);
+  BulkQuote bulk("c++ primer", 80.0, 10, 0.8);
+  LimitedQuote limit("c++ primer", 80.0, 10, 0.8);
+
+  print_total(std::cout, basic, 20);
+  print_total(std::cout, bulk, 20);
+  print_total(std::cout, limit, 20);
+
+  return 0;
+}
